0779c8dc65ddc701d466fd9ee7f781691c9ae75d
Matthew DiCicco <matthew.dicicco38@myhunter.cuny.edu>
2021-08-19 17:33:22 -0400

    Update EDA_video3_screencast.py

--- EDA_video3_screencast.py
+++ EDA_video3_screencast.py
@@ -1,15 +1,10 @@
 
-# coding: utf-8
-
-# In[2]:
 
 
 pd.set_option('max_columns', 100)
 
 
-# # Load the data
 
-# In[3]:
 
 
 train = pd.read_csv('./train.csv')
@@ -18,7 +13,7 @@ train.head()
 
 # # Build a quick baseline
 
-# In[4]:
+
 
 
 from sklearn.ensemble import RandomForestClassifier
@@ -41,7 +36,7 @@ rf = RandomForestClassifier()
 rf.fit(X,y)
 
 
-# In[5]:
+
 
 
 plt.plot(rf.feature_importances_)
@@ -50,7 +45,7 @@ plt.xticks(np.arange(X.shape[1]), X.columns.tolist(), rotation=90);
 
 # There is something interesting about `x8`.
 
-# In[6]:
+
 
 
 # we see it was standard scaled, most likely, if we concat train and test, we will get exact mean=1, and std 1 
@@ -58,14 +53,14 @@ print 'Mean:', train.x8.mean()
 print 'std:', train.x8.std()
 
 
-# In[7]:
+
 
 
 # And we see that it has a lot of repeated values
 train.x8.value_counts().head(15)
 
 
-# In[8]:
+
 
 
 # It's very hard to work with scaled feature, so let's try to scale them back
@@ -77,7 +72,7 @@ x8_unique_sorted = np.sort(x8_unique)
 np.diff(x8_unique_sorted)
 
 
-# In[9]:
+
 
 
 # The most of the diffs are 0.04332159! 
@@ -89,13 +84,13 @@ np.diff(x8_unique_sorted)
 np.diff(x8_unique_sorted/0.04332159)
 
 
-# In[10]:
+
 
 
 (train.x8/0.04332159).head(10)
 
 
-# In[11]:
+
 
 
 # Ok, now we see .102468 in every value
@@ -104,7 +99,7 @@ np.diff(x8_unique_sorted/0.04332159)
 (train.x8/0.04332159 - .102468).head(10)
 
 
-# In[12]:
+
 
 
 # let's round them 
@@ -112,7 +107,6 @@ x8_int = (train.x8/0.04332159 - .102468).round()
 x8_int.head(10)
 
 
-# In[13]:
 
 
 # Ok, what's next? In fact it is not obvious how to find shift parameter, 
@@ -120,13 +114,11 @@ x8_int.head(10)
 # But ...
 
 
-# In[14]:
-
 
 x8_int.value_counts()
 
 
-# In[ ]:
+
 
 
 # do you see this -1968? Doesn't it look like a year? ... So my hypothesis is that this feature is a year of birth! 
@@ -134,13 +126,13 @@ x8_int.value_counts()
 # The hypothesis looks plausible, isn't it?
 
 
-# In[22]:
+
 
 
 (x8_int + 1968.0).value_counts().sort_index()
 
 
-# In[23]:
+
 
 
 # After the competition ended the organisers told it was really a year of birth
